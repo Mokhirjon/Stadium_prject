4e6365f9c2077da2f43868a54469bd453a7ef3a4
abdulazizyuldashev11dev <abdulazizaziz7777@gmail.com>
2023-10-10 15:06:31 +0500

    admin

--- bron_stadium/admin.py
+++ bron_stadium/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import BronModel
+
+class BronModelAdmin(admin.ModelAdmin):
+    list_display = ('bron_name', 'begin_time', 'end_time', 'free_time', 'broned')
+    search_fields = ['bron_name', 'broned']
+
+admin.site.register(BronModel,BronModelAdmin)
