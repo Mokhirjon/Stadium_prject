349ae7bf8ffb3c1eee6864ee0beacd377603f9b5
abdulazizyuldashev11dev <abdulazizaziz7777@gmail.com>
2023-10-10 14:51:35 +0500

    url_finished

--- stadium/urls.py
+++ stadium/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('stadium/all/', StadiumListAPIView.as_view()),
-    path('stadium/create/', StadiumCreateAPIView.as_view()),
+    path('all/', StadiumListAPIView.as_view()),
+    path('create/', StadiumCreateAPIView.as_view()),
     path('update_stadium/<int:pk>/', StadiumUpdateAPIView.as_view()),
 ]
