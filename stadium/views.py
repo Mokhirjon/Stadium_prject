5068d16a479a6f2e4cb463956b9b671e45045f9e
abdulazizyuldashev11dev <abdulazizaziz7777@gmail.com>
2023-10-10 14:45:13 +0500

    f

--- stadium/views.py
+++ stadium/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from .models import StadiumModel
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from .serializers import *
+from config.permissions import AdminPermissionClass, OwnerPermissionClass, BronerPermissionClass
+
+class StadiumListAPIView(generics.ListAPIView):
+    queryset = StadiumModel.objects.all()
+    serializer_class = StadiumSerializer
+    permission_classes = (IsAuthenticated, AdminPermissionClass | OwnerPermissionClass | BronerPermissionClass)
+
+class StadiumCreateAPIView(generics.CreateAPIView):
+    queryset = StadiumModel.objects.all()
+    serializer_class = StadiumSerializer
+    permission_classes = (IsAuthenticated, AdminPermissionClass | OwnerPermissionClass)
+
+class StadiumUpdateAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = StadiumModel.objects.all()
+    serializer_class = StadiumSerializer
+    permission_classes = (IsAuthenticated, AdminPermissionClass | OwnerPermissionClass)
