eccd6404c0f338c5acae051f6863d29ee924d87d
abdulazizyuldashev11dev <abdulazizaziz7777@gmail.com>
2023-10-10 16:51:55 +0500

    u,v

--- bron_stadium/views.py
+++ bron_stadium/views.py
@@ -6,13 +6,15 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import BronModel
 from .serializers import BronSerializer
 from config.permissions import AdminPermissionClass, OwnerPermissionClass, BronerPermissionClass
+from rest_framework import filters
 
 # Category
 class BronStadiumListAPIView(generics.ListAPIView):
     queryset = BronModel.objects.all()
     serializer_class = BronSerializer
     permission_classes = (IsAuthenticatedOrReadOnly, AdminPermissionClass | OwnerPermissionClass | BronerPermissionClass)
-
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['bron_name', 'free_time']
 class BronStadiumCreateAPIView(generics.CreateAPIView):
     queryset = BronModel.objects.all()
     serializer_class = BronSerializer
--- stadium/views.py
+++ stadium/views.py
@@ -7,12 +7,14 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .serializers import *
 from config.permissions import AdminPermissionClass, OwnerPermissionClass, BronerPermissionClass
+from django_filters.rest_framework import DjangoFilterBackend
 
 class StadiumListAPIView(generics.ListAPIView):
     queryset = StadiumModel.objects.all()
     serializer_class = StadiumSerializer
     permission_classes = (IsAuthenticatedOrReadOnly, AdminPermissionClass | OwnerPermissionClass | BronerPermissionClass)
-
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['stadium_name', 'stadium_address']
 class StadiumCreateAPIView(generics.CreateAPIView):
     queryset = StadiumModel.objects.all()
     serializer_class = StadiumSerializer
