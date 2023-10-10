1d8c62705e117248df9332f063d0c9fdc9a76085
abdulazizyuldashev11dev <abdulazizaziz7777@gmail.com>
2023-10-10 15:21:10 +0500

    admin

--- stadium/admin.py
+++ stadium/admin.py
@@ -3,9 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from .models import StadiumModel
 from config.forms import Stadiumforms
-class BronModelAdmin(admin.ModelAdmin):
+class StadiumModelAdmin(admin.ModelAdmin):
     form = Stadiumforms
     list_display = ('stadium_name', 'stadium_address', 'contact', 'stadium_image', 'Bron_price', 'stadium_bio', 'stadium_rules', 'user')
     search_fields = ['stadium_name', 'stadium_address', 'contact']
 
-admin.site.register(StadiumModel,Stadiumforms)
+admin.site.register(StadiumModel,StadiumModelAdmin)
